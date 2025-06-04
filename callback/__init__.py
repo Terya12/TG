c9c0ca623867622543fd0cd600c6b3f46679fe7f
Olexandr Tereshchenko <Terya77812@gmail.com>
2025-06-04 12:42:03 +0300

    add basket router

--- callback/__init__.py
+++ callback/__init__.py
@@ -1,6 +1,10 @@
 from aiogram import Router
 from .orders import router as callback_order_router
+from .basket import router as basket_router
 
 router = Router(name=__name__)
 
-router.include_routers(callback_order_router)
+router.include_routers(
+    callback_order_router,
+    basket_router,
+)
