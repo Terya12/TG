c27b7755c6e000c9f692f4af6bdc016883347062
Olexandr Tereshchenko <Terya77812@gmail.com>
2025-06-06 20:55:53 +0300

    add payment router

--- callback/__init__.py
+++ callback/__init__.py
@@ -1,10 +1,13 @@
 from aiogram import Router
 from .orders import router as callback_order_router
 from .basket import router as basket_router
+from .payment import router as payment_router
+
 
 router = Router(name=__name__)
 
 router.include_routers(
     callback_order_router,
     basket_router,
+    payment_router,
 )
